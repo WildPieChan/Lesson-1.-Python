5ee0ac12b573533c99b219202ddd7414cc8abf8e
Kuksa <kirkfanton.kb@gmail.com>
2022-11-10 21:09:30 +0300

    Second task complete

---
+++ Program_2.py
@@ -0,0 +1,8 @@
+# Write a program for verification of the truth of the statemen 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z for all values of the predicate.
+
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
